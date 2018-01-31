4351a950baa17fd3b788708748a2b1f3fa69b7c4
lizhsen <lizhsen@163.com>
2018-01-31 16:24:51 +0800

    20180131

---
+++ apps/users/adminx.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import xadmin
+
+from .models import EmailVerifyRecord, Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ["code", "email", "send_type"]
+    search_fields = ["code", "email", "send_type"]
+    list_filter = ["code", "email", "send_type"]
+
+
+class BannerAdmin(object):
+    list_display = ["title", "image", "url", "index", "add_time"]
+    search_fields = ["title", "image", "url", "index"]
+    list_filter = ["title", "image", "url", "index", "add_time"]
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
