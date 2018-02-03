e014978c5339d35d8ae26a9361ac8e3a6a8a4f09
lizhsen <lizhsen@163.com>
2018-02-03 16:26:17 +0800

    modify one widgets.py for latter django version " line 76~77 input_html = [ht for ht in super(AdminSplitDateTime, self).render(name, value, attrs).split('/><') if ht != '']             input_html[0] = input_html[0] + "/>"             input_html[1] = "<" + input_html[1] "
    Signed-off-by: lizhsen <lizhsen@163.com>

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 import xadmin
 from django.views.generic import TemplateView
 from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, ModifyPwdView
+from organization.views import OrgView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
@@ -26,7 +27,8 @@ urlpatterns = [
     url(r'^register/$', RegisterView.as_view(), name="register"),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^active/(?P<active_code>.*)/$', ActiveUserView.as_view(), name='user_acitve'),
-    url(r'^forget/$', ForgetPwdView.as_view(), name='forget_pwd'),
+    url(r'^forget_pwd/$', ForgetPwdView.as_view(), name='forget_pwd'),
     url(r'^reset/(?P<active_code>.*)/$', ResetView.as_view(), name='reset_pwd'),
-    url(r'^modify/$', ModifyPwdView.as_view(), name='modify_pwd')
+    url(r'^modify/$', ModifyPwdView.as_view(), name='modify_pwd'),
+    url(r'^org_list/$', OrgView.as_view(), name='org_list')
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class OrgView(View):
+    """
+    课程机构列表功能
+    """
+    def get(self, request):
+        return render(request, "org-list.html", {})
